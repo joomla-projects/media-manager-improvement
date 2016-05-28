720bdf050631fe96f2a48b0050e8bd946ee02e15
Jisse Reitsma <info@yireo.com>
2016-05-28 08:49:01 +0200

    Move upload form to new template

--- administrator/components/com_media/views/folders/tmpl/default.php
+++ administrator/components/com_media/views/folders/tmpl/default.php
@@ -40,7 +40,6 @@ $doc->addScriptDeclaration("
 		</div>
 	</div>
 	<div class="span10">
-		<?php //echo $this->loadTemplate('navigation'); ?>
 		<?php if (($user->authorise('core.create', 'com_media')) && $this->require_ftp) : ?>
 			<form action="index.php?option=com_media&amp;task=ftpValidate" name="ftpForm" id="ftpForm" method="post">
 				<fieldset title="<?php echo JText::_('COM_MEDIA_DESCFTPTITLE'); ?>">
@@ -64,26 +63,8 @@ $doc->addScriptDeclaration("
 		</form>
 
 		<?php if ($user->authorise('core.create', 'com_media')): ?>
-			<!-- File Upload Form -->
 			<div id="collapseUpload" class="collapse">
-				<form
-					action="<?php echo JUri::base(); ?>index.php?option=com_media&amp;task=file.upload&amp;tmpl=component&amp;<?php echo $this->session->getName() . '=' . $this->session->getId(); ?>&amp;<?php echo JSession::getFormToken(); ?>=1&amp;format=html"
-					id="uploadForm" class="form-inline" name="uploadForm" method="post" enctype="multipart/form-data">
-					<div id="uploadform">
-						<fieldset id="upload-noflash" class="actions">
-							<label for="upload-file"
-							       class="control-label"><?php echo JText::_('COM_MEDIA_UPLOAD_FILE'); ?></label>
-							<input type="file" id="upload-file" name="Filedata[]" multiple/>
-							<button class="btn btn-primary" id="upload-submit"><span
-									class="icon-upload icon-white"></span> <?php echo JText::_('COM_MEDIA_START_UPLOAD'); ?>
-							</button>
-							<p class="help-block"><?php echo $this->config->get('upload_maxsize') == '0' ? JText::_('COM_MEDIA_UPLOAD_FILES_NOLIMIT') : JText::sprintf('COM_MEDIA_UPLOAD_FILES', $this->config->get('upload_maxsize')); ?></p>
-						</fieldset>
-						<input class="update-folder" type="hidden" name="folder" id="folder"
-						       value="<?php echo $this->state->folder; ?>"/>
-						<?php $this->session->set('com_media.return_url', 'index.php?option=com_media'); ?>
-					</div>
-				</form>
+				<?php echo $this->loadTemplate('upload'); ?>
 			</div>
 			<div id="collapseFolder" class="collapse">
 				<form
