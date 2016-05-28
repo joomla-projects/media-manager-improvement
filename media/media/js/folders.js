0b33f4e918d522911ca280b3420ab6bc3a6e21e4
Jisse Reitsma <info@yireo.com>
2016-05-28 10:43:52 +0200

    Mark current folder as active; Change address bar on click

--- media/media/js/folders.js
+++ media/media/js/folders.js
@@ -1,60 +1,63 @@
 /**
- * @copyright	Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright    Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license        GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 /**
  * JMediaManager behavior for media component
  *
- * @package		Joomla.Extensions
+ * @package        Joomla.Extensions
  * @subpackage  Media
- * @since		3.5
+ * @since        3.5
  */
-jQuery(document).ready(function($){
-	$('a').each(function(index, value){
-		if ($(this).hasClass("ajaxInit")) {
-			$(this).click(function(e){
-				e.preventDefault();
-				var relDir = $(this).data('href');
+jQuery(document).ready(function ($) {
+    $('a').each(function (index, value) {
+        if ($(this).hasClass("ajaxInit")) {
+            $(this).click(function (e) {
+                e.preventDefault();
+                var relDir = $(this).data('href');
 
-				// Ajax call to reload the #filesview div with the files on that folder
-				var request = $.ajax({
-					url: 'index.php?option=com_media',
-					method: "GET",
-					data: { folder : relDir, tmpl : 'component',  format : 'raw', view : 'files'},
-					dataType: "html"
-				});
+                // Show this folder as active
+                $('a.ajaxInit').removeClass('active');
+                $(this).addClass('active');
 
-				request.done(function( msg ) {
-					$( "#filesview" ).html( msg );
-				});
+                // Push this information into the address bar
+                window.history.pushState(null, null, "index.php?option=com_media&view=folders&folder=" + relDir);
 
-				request.fail(function( jqXHR, textStatus ) {
-					alert( "Request failed: " + textStatus );
-				});
-				console.log(relDir);
-			});
-		}
-	});
+                // Ajax call to reload the #filesview div with the files on that folder
+                var request = $.ajax({
+                    url: 'index.php?option=com_media',
+                    method: "GET",
+                    data: {folder: relDir, tmpl: 'component', format: 'raw', view: 'files'},
+                    dataType: "html"
+                });
 
-	console.log(basepath);
-	if (basepath === 'images')
-	{
-		// Ajax call to reload the #filesview div with the files on that folder
-		var request = $.ajax({
-			url: 'index.php?option=com_media&view=files&tmpl=component&format=raw',
-			method: "GET",
-			data: { folder : "" },
-			dataType: "html"
-		});
+                request.done(function (msg) {
+                    $("#filesview").html(msg);
+                });
 
-		request.done(function( msg ) {
-			$( "#filesview" ).html( msg );
-		});
+                request.fail(function (jqXHR, textStatus) {
+                    alert("Request failed: " + textStatus);
+                });
+            });
+        }
+    });
 
-		request.fail(function( jqXHR, textStatus ) {
-			alert( "Request failed: " + textStatus );
-		});
-		console.log(basepath);
-	}
+    if (basepath === 'images') {
+        // Ajax call to reload the #filesview div with the files on that folder
+        var request = $.ajax({
+            url: 'index.php?option=com_media&view=files&tmpl=component&format=raw',
+            method: "GET",
+            data: { folder : "" },
+            dataType: "html"
+        });
+
+        request.done(function (msg) {
+            $("#filesview").html(msg);
+        });
+
+        request.fail(function (jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
+    }
 });
