c3dadff4c02441f977479b546530106073f4ff7f
David Neukirchen <david.neukirchen@gmail.com>
2016-12-23 22:00:51 +0100

    Make the condition more readable

--- administrator/components/com_media/media.php
+++ administrator/components/com_media/media.php
@@ -28,30 +28,34 @@ if (!$user->authorise('core.manage', 'com_media') && (!$asset || (!$user->author
 
 // Show the new media manager for browsers that do support ECMAScript 2015
 $client = JFactory::getApplication()->client;
-if ($client->browser != WebClient::IE || $client->version > 8)
+
+if ($client->browser == WebClient::IE && $client->version <= 8)
+{
+	// Load the helper class
+	JLoader::register('MediaHelper', JPATH_ADMINISTRATOR . '/components/com_media/legacy/helpers/media.php');
+
+	// Get the component parameters
+	$params = JComponentHelper::getParams('com_media');
+
+	// Set the path definitions
+	$popup_upload = $input->get('pop_up', null);
+	$path         = 'file_path';
+	$view         = $input->get('view');
+
+	if (substr(strtolower($view), 0, 6) == 'images' || $popup_upload == 1)
+	{
+		$path = 'image_path';
+	}
+
+	define('COM_MEDIA_BASE', JPATH_ROOT . '/' . $params->get($path, 'images'));
+	define('COM_MEDIA_BASEURL', JUri::root() . $params->get($path, 'images'));
+
+	$controller = JControllerLegacy::getInstance('Media', array('base_path' => JPATH_COMPONENT_ADMINISTRATOR . '/legacy'));
+	$controller->execute($input->get('task'));
+
+	return $controller->redirect();
+}
+else
 {
 	// TODO instantiate the new media manager here
 }
-
-// Load the helper class
-JLoader::register('MediaHelper', JPATH_ADMINISTRATOR . '/components/com_media/legacy/helpers/media.php');
-
-// Get the component parameters
-$params = JComponentHelper::getParams('com_media');
-
-// Set the path definitions
-$popup_upload = $input->get('pop_up', null);
-$path         = 'file_path';
-$view         = $input->get('view');
-
-if (substr(strtolower($view), 0, 6) == 'images' || $popup_upload == 1)
-{
-	$path = 'image_path';
-}
-
-define('COM_MEDIA_BASE', JPATH_ROOT . '/' . $params->get($path, 'images'));
-define('COM_MEDIA_BASEURL', JUri::root() . $params->get($path, 'images'));
-
-$controller = JControllerLegacy::getInstance('Media', array('base_path' => JPATH_COMPONENT_ADMINISTRATOR . '/legacy'));
-$controller->execute($input->get('task'));
-$controller->redirect();
