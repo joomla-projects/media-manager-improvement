aed16e04fd58c1426804dab1e4c87e7279d7ab3a
David Neukirchen <david.neukirchen@gmail.com>
2017-02-12 22:09:17 +0100

    Implement folder navigation

--- administrator/components/com_media/resources/components/browser/items/directory.vue
+++ administrator/components/com_media/resources/components/browser/items/directory.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div @click="select(item)"
+         @dblclick="open(item.path)">
         <p class="media-browser-icon">
             <i class="icon icon-folder-close"></i>
         </p>
@@ -8,10 +9,18 @@
         </p>
     </div>
 </template>
-
 <script>
     export default {
         name: 'media-browser-item-directory',
         props: ['item'],
+        methods: {
+            open: function (path) {
+                Media.Event.fire('dirChanged', path);
+            },
+            select: function(item) {
+                // TODO implement
+                // Media.Event.fire('itemSelected', item);
+            }
+        }
     }
 </script>
