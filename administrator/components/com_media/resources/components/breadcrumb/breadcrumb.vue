35232dffd3c4ab3038ba336bb6b0a85c245fecf2
David Neukirchen <david.neukirchen@gmail.com>
2017-02-12 21:57:05 +0100

    Add breadcrumb navigation

--- administrator/components/com_media/resources/components/breadcrumb/breadcrumb.vue
+++ administrator/components/com_media/resources/components/breadcrumb/breadcrumb.vue
@@ -1,15 +1,21 @@
 <template>
     <ul class="breadcrumb media-breadcrumb">
         <li>
-            <a href="#">Root</a>
+            <a @click.stop.prevent="goTo('/')">Root</a>
             <span class="divider">/</span>
         </li>
         <li v-for="item in directories">
-            <a href="#">{{ item }}</a>
+            <a @click.stop.prevent="goTo(item.path)">{{ item.name }}</a>
             <span class="divider">/</span>
         </li>
     </ul>
 </template>
+
+<style>
+    .breadcrumb a {
+        cursor: pointer;
+    }
+</style>
 
 <script>
     export default {
@@ -17,8 +23,22 @@
         props: ['dir'],
         computed: {
             directories: function () {
-                return this.dir.split('/').filter((el) => el.length !== 0);
+                const items = [];
+                this.dir.split('/')
+                    .filter((crumb) => crumb.length !== 0)
+                    .forEach((crumb) => {
+                        items.push({
+                            name: crumb,
+                            path: this.dir.split(crumb)[0] + '/' + crumb,
+                        });
+                    })
+                return items;
             }
-        }
+        },
+        methods: {
+            goTo: function (path) {
+                Media.Event.fire('dirChanged', path);
+            }
+        },
     }
 </script>
