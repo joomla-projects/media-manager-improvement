304470946d3e20a03f3bf9fbcb282486c9db6860
David Neukirchen <david.neukirchen@gmail.com>
2017-02-22 19:33:42 +0100

    Load assets in template & use JHtml::_ magic helper

--- administrator/components/com_media/views/media/view.html.php
+++ administrator/components/com_media/views/media/view.html.php
@@ -26,50 +26,11 @@ class MediaViewMedia extends JViewLegacy
 	 */
 	public function display($tpl = null)
 	{
-		// Prepare the document
-		$this->prepareDocument();
 
 		// Prepare the toolbar
 		$this->prepareToolbar();
 
 		parent::display($tpl);
-	}
-
-	/**
-	 * Prepare the document.
-	 *
-	 * @return  void
-	 *
-	 * @since   __DEPLOY_VERSION__
-	 */
-	protected function prepareDocument()
-	{
-		$doc    = JFactory::getDocument();
-		$params = JComponentHelper::getParams('com_media');
-
-		// Make sure core.js is loaded before media scripts
-		JHtml::_('behavior.core');
-
-		// Populate the media config
-		$config = array(
-			'apiBaseUrl'              => JUri::root() . 'administrator/index.php?option=com_media&format=json',
-			'filePath'                => $params->get('file_path', 'images'),
-			'allowedUploadExtensions' => $params->get('upload_extensions', ''),
-			'maxUploadSizeMb'         => $params->get('upload_maxsize', 10),
-		);
-		$doc->addScriptOptions('com_media', $config);
-
-		// Populate the language
-		// TODO use JText for all language strings used by the js application
-
-		// Add javascripts
-		$doc->addScript(JUri::root() . 'media/com_media/js/mediamanager.js');
-
-		// Add stylesheets
-		$doc->addStyleSheet(JUri::root() . 'media/com_media/css/mediamanager.css');
-
-		// TODO include the font in the component media (self hosted)
-		$doc->addStyleSheet('https://fonts.googleapis.com/icon?family=Material+Icons');
 	}
 
 	/**
