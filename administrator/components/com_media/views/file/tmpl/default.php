5436daa5786ca4a08df76d8a876c2a111a96dd1d
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2017-04-02 01:49:20 +0300

    more

--- administrator/components/com_media/views/file/tmpl/default.php
+++ administrator/components/com_media/views/file/tmpl/default.php
@@ -12,14 +12,20 @@ defined('_JEXEC') or die;
 // Add javascripts
 JHtml::_('behavior.formvalidator');
 
-JHtml::_('script', 'vendors/eventbausjs/eventbus.min.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'vendor/eventbusjs/eventbus.min.js', array('version' => 'auto', 'relative' => true));
 JHtml::_('script', 'com_media/edit.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'com_media/toblob.js', array('version' => 'auto', 'relative' => true));
 
 /**
  * @var JForm $form
  */
 $form = $this->form;
 ?>
+<style>
+	.btn-group {
+		display: block;
+	}
+</style>
 <form action="#" method="post" name="adminForm" id="media-form" class="form-validate">
 <?php
 $fieldSets = $form->getFieldsets();
@@ -32,9 +38,15 @@ if ($fieldSets)
 
 	echo JHtml::_('bootstrap.endTabSet');
 }
+// @TODO Logic for handling other types of media, not only images!!!!!
 ?>
+<input type="hidden" name="<?php echo JFactory::getSession()->getToken(); ?>" value="1">
 </form>
-
+<p>Edit area: </p>
 <span class="image-container">
-    <img id="media-edit-file" src="<?php echo $this->fullFilePath ?>"/>
+    <img id="media-edit-file" src="<?php echo $this->fullFilePath ?>" width="100%"/>
+</span>
+<p>Preview: </p>
+<span class="image-container-preview">
+    <img id="media-edit-file-new" src="<?php echo $this->fullFilePath ?>" style="max-width:100%"/>
 </span>
