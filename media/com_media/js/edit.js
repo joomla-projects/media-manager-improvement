846da23ecad371be2d0a5ebef898c6a4a529650c
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2017-04-03 17:17:10 +0300

    Edit part

--- media/com_media/js/edit.js
+++ media/com_media/js/edit.js
@@ -1,16 +1,173 @@
+// OUR CODE
+// NEEDS CORE.JS
 (function() {
 	"use strict";
 
-	document.addEventListener('DOMContentLoaded', function() {
-		var list = document.querySelectorAll('a[data-toggle="tab"]');
+	// The upload object
+	Joomla.UploadFile = {};
+	Joomla.UploadFile.removeProgressBar = function() {
+		setTimeout(function() {
+//			document.querySelector('#jloader').outerHTML = "";
+//			delete document.querySelector('#jloader');
+//			document.querySelector('.media-browser').style.borderWidth = '1px';
+//			document.querySelector('.media-browser').style.borderStyle = 'solid';
+		}, 200);
+	};
 
-		list.forEach(function(item) {
-			item.addEventListener('shown.bs.tab', function(event) {
-				if (event.relatedTarget) {
-					EventBus.dispatch('onDeactivate', this, event.relatedTarget.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
+	/**
+	 *
+	 * @param name        the name of the file
+	 * @param file       the file
+	 * @param uploadPath the file
+	 * @param url        the file
+	 * @param type       the file
+	 * @constructor
+	 */
+	Joomla.UploadFile.exec = function (name, file, uploadPath, url, type) {
+
+		var reader = new FileReader();
+
+		reader.onload = function() {
+			window.file = reader.result;
+		};
+		reader.readAsText(file)
+
+		var forUpload = {
+			'name': name,
+			'content': window.file ///file.replace(/data:+.+base64,/, '')
+		};
+
+
+// @TODO get these from the store
+		uploadPath = '';
+		url = '/administrator/index.php?option=com_media&task=api.files&format=json&path=' + uploadPath + '/' + name + '&' +document.querySelector('input[type="hidden"][value="1"]').name+'=1';
+		type = 'application/json';
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.upload.onprogress = function(e) {
+			var percentComplete = (e.loaded / e.total) * 100;
+			//document.dispatchEvent(new Event('upload.progress', {"progress": percentComplete + '%'}));
+		};
+
+		xhr.onload = function() {
+			try {
+				var resp = JSON.parse(xhr.responseText);
+			} catch (e) {
+				var resp = null;
+			}
+
+			if (resp) {
+				if (xhr.status == 200) {
+					if (resp.success == true) {
+						item =resp.data;
+						Joomla.UploadFile.removeProgessBar();
+					}
+
+					if (resp.status == '1') {
+						Joomla.renderMessages({'success': [resp.message]}, 'true');
+						Joomla.UploadFile.removeProgessBar();
+					}
 				}
-				EventBus.dispatch('onActivate', this, event.target.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
-			})
+			} else {
+				Joomla.UploadFile.removeProgessBar();
+			}
+		};
+
+		xhr.onerror = function() {
+			Joomla.UploadFile.removeProgessBar();
+		};
+
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader('Content-Type', type);
+		xhr.send(JSON.stringify(forUpload));
+	};
+
+
+
+	document.addEventListener('DOMContentLoaded', function() {
+
+		// Upload cropped image to server
+		var doTheUpload = function (mediaFile) {
+			var filename = document.getElementById('media-edit-file').src.split('/');
+			filename = filename[filename.length -1];
+			var blobBin = atob(filename);
+			var array = [];
+			for(var i = 0; i < blobBin.length; i++) {
+				array.push(blobBin.charCodeAt(i));
+			}
+
+			var file=new Blob([new Uint8Array(array)], {type: 'image/png'});
+
+			Joomla.UploadFile.exec(filename, file);
+			// This Joomla.cropper.getCroppedCanvas() needs to be the media file
+			// Joomla.cropper.getCroppedCanvas().toBlob(function (blob) {
+			// 	var imgFileName = document.getElementById('media-edit-file').src.split('/').pop();
+			//
+			// 	Joomla.UploadFile.exec(imgFileName, blob);
+			// });
+		};
+
+		// This needs a good refactoring once we'll get the new UI/CE
+		// Crap to satisfy jQuery's slowlyness!!!
+		var func = function() {
+				jQuery('[data-toggle="tab"]').on('shown.bs.tab', function(event) {
+					if (event.relatedTarget) {
+						EventBus.dispatch('onDeactivate', this, event.relatedTarget.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
+					}
+					EventBus.dispatch('onActivate', this, event.target.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
+				});
+
+			jQuery('[data-toggle="tab"]').click();
+			jQuery('[data-toggle="tab"]')[0].click();
+		};
+		setTimeout(func, 1000); // jQuery...
+
+		// jQuery(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+		// 	if (e.relatedTarget) {
+		// 		EventBus.dispatch('onDeactivate', this, e.relatedTarget.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
+		// 	}
+		// 	EventBus.dispatch('onActivate', this, e.target.hash.replace('#attrib-', ''), document.getElementById('media-edit-file'));
+		// });
+
+		/**
+		 * Toolbar custom functionality
+		 */
+		var close, save, subHead = document.getElementById('subhead'),
+		    toolbarButtons = [].slice.call(subHead.querySelectorAll('.btn.btn-sm'));
+
+		close = function(event) {
+			event.preventDefault();
+			var pathName = window.location.pathname.replace(/&view=file.*/g, '');
+			window.location = pathName + '?option=com_media';
+		};
+
+		save = function(event) {
+			event.preventDefault();
+			// Do the upload
+			doTheUpload();
+
+		};
+
+		toolbarButtons.forEach(function(item) {
+			// Cancel
+			if (item.classList.contains('btn-danger')) {
+				item.removeAttribute('onclick');
+				item.addEventListener('click', close);
+			}
+
+			// Save
+			if (item.classList.contains('btn-success')) {
+				item.removeAttribute('onclick');
+				item.addEventListener('click', save);
+			}
+
+			// save and close
+			if (item.classList.contains('btn-outline-success')) {
+				item.removeAttribute('onclick');
+				item.addEventListener('click', save);
+				item.addEventListener('click', close);
+			}
 		})
 	});
 })();
--- media/plg_media-action_crop/js/crop.js
+++ media/plg_media-action_crop/js/crop.js
@@ -2,27 +2,87 @@
 	"use strict";
 
 	document.addEventListener('DOMContentLoaded', function() {
-		EventBus.addEventListener('onActivate', function(e, context, imageElement){
-			if (context != 'crop') {
-				return;
-			}
 
-			var cropper = new Cropper(imageElement, {
+		var init = function(imageElement) {
+			// Clear previous cropper
+			if (Joomla.cropper) Joomla.cropper = {};
+
+			// Initiate the cropper
+			Joomla.cropper = new Cropper(imageElement, {
+				viewMode: 1,
+				responsive:true,
+				restore:true,
+				autoCrop:false,
+				minContainerWidth: imageElement.offsetWidth,
 				minContainerHeight: imageElement.offsetHeight,
 				crop: function(e) {
 					document.getElementById('jform_crop_x').value = e.detail.x;
 					document.getElementById('jform_crop_y').value = e.detail.y;
 					document.getElementById('jform_crop_width').value = e.detail.width;
 					document.getElementById('jform_crop_height').value = e.detail.height;
+
+					Joomla.cropper.getCroppedCanvas().toBlob(function (blob) {
+						var imgFileName = document.getElementById('media-edit-file').src.split('/').pop();
+						// console.log(imgFileName)
+						// console.log(blob)
+
+						// Make sure that the plugin didn't remove the preview
+						if (document.getElementById('media-edit-file-new')) {
+							document.getElementById('media-edit-file-new').src = URL.createObjectURL(blob);
+						} else {
+							// Use createObjectURL to make a URL for the blob
+							var image = new Image();
+							image.id = 'media-edit-file-new';
+							image.src = URL.createObjectURL(blob);
+							document.body.appendChild(image);
+						}
+
+						// Do the upload automatically????
+						// Joomla.UploadFile.exec(imgFileName, blob);
+					});
 				}
 			});
+
+
+
+			// @TODO Actions as Buttons
+			// var actions = document.getElementById('jform_aspectRatio'), buttons = [].slice.call(actions.querySelectorAll('[type="radio"]'));
+			//
+			// buttons.forEach(function(item) {
+			// 	item.addEventListener('click', function(event) {
+			// 		console.log(event.target.value);
+			// 		Joomla.cropper.setAspectRatio = event.target.value;
+			// 	})
+			// });
+
 
 			document.getElementById('jform_crop_x').value = 0;
 			document.getElementById('jform_crop_y').value = 0;
 			document.getElementById('jform_crop_width').value = imageElement.offsetWidth;
 			document.getElementById('jform_crop_height').value = imageElement.offsetHeight;
 
-			imageElement.cropper = cropper;
+
+			//imageElement.cropper = cropper;
+		};
+
+		EventBus.addEventListener('onActivate', function(e, context, imageElement){
+			// Bail out early
+			if (context != 'crop') {
+				return;
+			}
+
+			// Initialize
+			init(imageElement);
+
+			// Reset on resize
+			// window.addEventListener('resize', function(event) {
+			// 	// console.log('hit')
+			// 	// Joomla.cropper.reset();
+			// 	Joomla.cropper.minContainerWidth = document.querySelector('span.image-container').offsetWidth;
+			// 		// Joomla.cropper.minContainerHeight = document.querySelector('span.image-container').clientWidth;
+			// 	document.getElementById('media-edit-file-new').src = document.getElementById('media-edit-file').src;
+			// });
+
 		});
 
 		EventBus.addEventListener('onDeactivate', function(e, context, imageElement){
@@ -34,3 +94,131 @@
 		});
 	}, false);
 })();
+
+/**
+ *
+!function() {
+	"use strict";
+
+	document.onreadystatechange = function () {
+		if (document.readyState == "interactive") {
+			var image = document.getElementById('joomla-media-image-cropper');      // The image node
+			window.imageUrl = image.getAttribute("src");                            // The image Url
+			window.postUrl = image.getAttribute("data-url");                        // The upload Url
+			window.submitInput = document.querySelectorAll('input[type="submit"]'); // The hidden submit input
+			window.cropperBoxDim = {};
+
+			// TODO get any data-* values and build the options
+			// eg:   if (typeof image.getAttribute("data-some-attribute") != "undefined") {
+			//           option1 = image.getAttribute("data-some-attribute");
+			//       }
+
+			/**
+			 * Initialiaze Cropper
+			 *
+			var cropper = new Cropper(image, {
+				// aspectRatio: 16 / 9,
+				crop       : function (e) {
+
+					var json = [{
+						"x"     : e.detail.x,
+						"y"     : e.detail.y,
+						"height": e.detail.height,
+						"width" : e.detail.width,
+						"rotate": e.detail.rotate,
+						"scaleX": e.detail.scaleX,
+						"scaleY": e.detail.scaleY
+					}];
+
+					document.getElementById('imagecropper-jsondata').value = json;
+				}
+			});
+			console.log(cropper);
+			// The upload logic copy paste from tinymce jdrag and drop
+			var UploadFile = function (fd) {
+				var xhr = new XMLHttpRequest();
+				xhr.open("POST", postUrl, true);
+
+				xhr.onload = function () {
+					var resp = JSON.parse(xhr.responseText);
+
+					if (xhr.status == 200) {
+						if (resp.status == '0') {
+							submitInput[0].click();
+							// console.log('Upload success');
+						}
+
+						if (resp.status == '1') {
+							submitInput[0].click();
+							// console.log('Upload success');
+						}
+					} else {
+						// console.log('No Upload');
+					}
+				};
+
+				xhr.onerror = function () {
+					// console.log('Upload Error');
+				};
+				xhr.send(fd);
+			};
+
+			// Upload cropped image to server
+			var doTheUpload = function () {
+				console.log(cropper.getCroppedCanvas());
+				cropper.getCroppedCanvas().toBlob(function (blob) {
+					var imgFileName = imageUrl.split('/').pop();
+					var fd = new FormData();
+
+					fd.append('files', blob, imgFileName);
+
+					UploadFile(fd);
+				});
+			};
+
+			var registerClick = function (element) {
+				element.addEventListener('click', function (event) {
+
+					var action = event.currentTarget.getAttribute("data-method");
+					var option = event.currentTarget.getAttribute("data-option");
+					var option2 = event.currentTarget.getAttribute("data-second-option");
+
+					switch (action) {
+						case 'zoom':
+							cropper.zoom(option);
+							break;
+
+						case 'rotate':
+							cropper.rotate(option);
+							break;
+
+						case 'scaleX':
+							cropper.scaleX(-cropper.getData().scaleX || -1);
+							break;
+
+						case 'scaleY':
+							cropper.scaleY(-cropper.getData().scaleY || -1);
+							break;
+
+						case 'move':
+							cropper.move(option, option2);
+							break;
+
+						default:
+						case 'crop':
+							console.log(cropper.getCroppedCanvas());
+							//cropper.getCroppedCanvas();
+							doTheUpload();
+							break;
+					}
+				});
+			};
+
+			var buttons = document.querySelectorAll('.btn');
+
+			for (var j = 0; j < buttons.length; j++) {
+				registerClick(buttons[j]);
+			}
+		}
+	}
+}(); */
