c839f857d378f2a6151ebe762f1064c613c65e00
kasun <alankasun@gmail.com>
2017-05-13 16:06:34 +0530

    Added move/copy support to API endpoint

--- administrator/components/com_media/models/api.php
+++ administrator/components/com_media/models/api.php
@@ -214,4 +214,36 @@ class MediaModelApi extends Model
 
 		return $name;
 	}
+
+    /**
+     * Copies file of folder from source path to destination path
+     * If forced, exciting files/folders would be overwritten
+     *
+     * @param   string  $sourcePath        Source path of the file or folder(relative)
+     * @param   string  $destinationPath   Destination path(relative)
+     * @param   bool    $force             Force to overwrite
+     *
+     * @since __DEPLOY_VERSION__
+     * @throws  Exception
+     */
+    public function copy($sourcePath, $destinationPath, $force = false)
+    {
+        $this->adapter->copy($sourcePath, $destinationPath, $force);
+	}
+
+    /**
+     * Moves file of folder from source path to destination path
+     * If forced, exciting files/folders would be overwritten
+     *
+     * @param   string  $sourcePath        Source path of the file or folder(relative)
+     * @param   string  $destinationPath   Destination path(relative)
+     * @param   bool    $force             Force to overwrite
+     *
+     * @since __DEPLOY_VERSION__
+     * @throws  Exception
+     */
+    public function move($sourcePath, $destinationPath, $force = false)
+    {
+        $this->adapter->move($sourcePath, $destinationPath, $force);
+	}
 }
