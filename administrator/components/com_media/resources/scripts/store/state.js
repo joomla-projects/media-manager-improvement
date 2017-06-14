dd08a10c3cdf22d6698e04c4794795686940aeee
David Neukirchen <david.neukirchen@gmail.com>
2017-06-14 09:12:42 +0200

    Less complex multi adapter example

--- administrator/components/com_media/resources/scripts/store/state.js
+++ administrator/components/com_media/resources/scripts/store/state.js
@@ -1,10 +1,24 @@
+// Get the disks from joomla option storage
+const options = Joomla.getOptions('com_media', {});
+if (options.providers === undefined || options.providers.length === 0) {
+    throw new TypeError('Media providers are not defined.');
+}
+
+
+console.log(options);
 // The initial state
 export default {
-    selectedDirectory: '/',
-    directories: [
-        {path: '/', name: 'PLACEHOLDER', directories: [], files: [], directory: null}
-    ],
+    // The loaded directories
+    directories: options.providers.map((disk) => {
+        return {path: disk.name + ':/', name: disk.displayName, directories: [], files: [], directory: null}
+    }),
+    // The loaded files
     files: [],
-    showCreateFolderModal: false,
+    // The selected disk. Providers are ordered by plugin ordering, so we set the first provider
+    // in the list as the default provider.
+    selectedDirectory: options.providers[0].name + ':/',
+    // The currently selected items
+    selectedItems: [],
+    // The currently selected items
     selectedItems: [],
 }
