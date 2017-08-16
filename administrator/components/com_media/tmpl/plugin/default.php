d0068cf9d4fa8c0f4961a316d46d559b50c45e3d
kasun <alankasun@gmail.com>
2017-08-16 07:00:51 +0530

    added license

--- administrator/components/com_media/tmpl/plugin/default.php
+++ administrator/components/com_media/tmpl/plugin/default.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Get the action from the input
+ * @package     Joomla.Administrator
+ * @subpackage  com_media
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
+defined('_JEXEC') or die;
+
+
+//Get the action from the input
 $action = JFactory::getApplication()->input->getWord('action');
 
 /**
