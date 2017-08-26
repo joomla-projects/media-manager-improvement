8c3f6752621b13b72b3c1d4a89de635a3b18a093
kasun <alankasun@gmail.com>
2017-08-26 22:48:08 +0530

    updated custom field type

--- plugins/filesystem/dropbox/field/authbutton.php
+++ plugins/filesystem/dropbox/field/authbutton.php
@@ -1,26 +1,71 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  FileSystem.Dropbox
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.form.formfield');
+\JLoader::import('filesystem.dropbox.vendor.autoload', JPATH_PLUGINS);
 
+/**
+ * Class JFormFieldAuthButton
+ *
+ * @since  __DEPLOY_VERSION__
+ */
 class JFormFieldAuthButton extends JFormField
 {
+	/**
+	 * Name of the field
+	 *
+	 * @var string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
 	protected $type = "AuthButton";
 
+	/**
+	 * Get label of the button
+	 *
+	 * @return string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
 	public function getLabel()
 	{
 		return '';
 	}
 
+	/**
+	 * Get input for button
+	 *
+	 * @return string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
 	public function getInput()
 	{
-		$formData  = $this->form->getData();
-		$formData  = $formData->data;
-		$app_secret = '';
+		$plugin = \Joomla\CMS\Plugin\PluginHelper::getPlugin('filesystem', 'dropbox');
+		$params = new \Joomla\Registry\Registry($plugin->params);
+		$client_id = $params->get('client_id', '');
+		$client_secret = $params->get('client_secret', '');
+
+		$app = new \Kunnu\Dropbox\DropboxApp($client_id, $client_secret);
+		$dropbox = new \Kunnu\Dropbox\Dropbox($app);
+
+		$helper = $dropbox->getAuthHelper();
+		$redirectUri = $helper->getAuthUrl('http://localhost/media-manager-improvement/administrator/index.php'
+		                                   .'?option=com_media&task=plugin.oauthcallback&plugin=dropbox');
 
 
-		$html   = '<a href="https://www.dropbox.com/oauth2/authorize?response_type=code&redirect_uri='. urlencode('http://localhost/media-manager-improvement/administrator/index.php?option=com_media&task=plugin.oauthcallback&plugin=dropbox') . '&client_id=' . $app_secret . '">Button</a>';
+		$html = '<a class="btn btn-info" href="'. $redirectUri .'">Get Access Token</a>';
+		$html .= '<br><p><b>*Use this button after saving client credentials</b></p>';
+
 		return $html;
 	}
 }
