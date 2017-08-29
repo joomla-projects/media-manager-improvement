95272859a666fc4c4edf1905920db1f41f28dad8
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2017-08-29 19:44:05 +0300

    respect adapters

--- administrator/components/com_media/resources/scripts/components/browser/items/item.js
+++ administrator/components/com_media/resources/scripts/components/browser/items/item.js
@@ -42,24 +42,23 @@ export default {
             return store.state.selectedItems.some(selected => selected.path === item.path);
         }
 
+	    /**
+	     * Create and dispatch onMediaFileSelected Event
+	     *
+	     * @param {object}  data  The data for the detail
+	     *
+	     * @returns {void}
+	     */
+        function sendEvent(data) {
+	        const ev = new CustomEvent('onMediaFileSelected', {"bubbles":true, "cancelable":false, "detail": data});
+	        window.parent.document.dispatchEvent(ev);
+        }
         /**
          * Handle the click event
          * @param event
          */
         function handleClick(event) {
-            const rootPath = Joomla.getOptions('com_media').fileBaseRelativeUrl;
-            const cloudRootPath = Joomla.getOptions('com_media').fileBaseRelativeUrl; //@todo return the cloud root...
-            const isCloud = false; //@todo return true if file is on the cloud
 	        let path = false;
-
-            if (item.type === 'file') {
-                if (isCloud) {
-                    path = cloudRootPath + item.path;
-                } else {
-	                path = rootPath + item.path;
-                }
-            }
-
 	        const data = {
 		        path: path,
 		        thumb: false,
@@ -67,9 +66,39 @@ export default {
 		        extension: item.extension ? item.extension : false,
 	        };
 
-	        const ev = new CustomEvent('onMediaFileSelected', {"bubbles":true, "cancelable":false, "detail": data});
+            if (item.type === 'file') {
+	            const csrf = Joomla.getOptions('com_media').csrfToken;
+	            const apiBaseUrl = Joomla.getOptions('com_media').apiBaseUrl;
+	            Joomla.request({
+		            url: `${apiBaseUrl}&task=api.files&url=true&path=${item.path}&${csrf}=1`,
+		            method: 'GET',
+		            perform: true,
+		            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		            onSuccess: (response) => {
+			            const resp = JSON.parse(response);
+			            if (resp.success === true) {
+				            if (resp.data[0].url) {
+					            if (/local-/.test(item.path)) {
+						            const server = Joomla.getOptions('system.paths').rootFull;
+						            const newPath = resp.data[0].url.split(server)[1];
 
-            window.parent.document.dispatchEvent(ev);
+						            data.path  = newPath;
+						            if (resp.data[0]['thumb_path'])
+						            data.thumb = resp.data[0].thumb_path;
+					            } else {
+						            data.path  = path;
+						            if (resp.data[0]['thumb_path'])
+						            data.thumb = resp.data[0].thumb_path;
+					            }
+				            }
+			            }
+			            sendEvent(data)
+		            },
+		            onError: () => {
+			            sendEvent(data)
+		            },
+	            });
+            }
 
             // Handle clicks when the item was not selected
             if (!isSelected()) {
