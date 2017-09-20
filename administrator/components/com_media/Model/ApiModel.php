cc67c75e52576dddf6f6c21039d879b8ebbf4860
kasun <alankasun@gmail.com>
2017-09-20 18:44:09 +0200

    fixups

--- administrator/components/com_media/Model/ApiModel.php
+++ administrator/components/com_media/Model/ApiModel.php
@@ -36,20 +36,6 @@ class ApiModel extends BaseModel
 	protected $providerManager = null;
 
 	/**
-	 * Constructor
-	 *
-	 * @param   array                $config   An array of configuration options (name, state, dbo, table_path, ignore_request).
-	 * @param   MvcFactoryInterface  $factory  The factory.
-	 *
-	 * @since   __DEPLOY_VERSION__
-	 * @throws  \Exception
-	 */
-	public function __construct($config = array(), MvcFactoryInterface $factory = null)
-	{
-		parent::__construct($config, $factory);
-	}
-
-	/**
 	 * Setup the adapters for Media Manager
 	 *
 	 * @return  void
