4bd4073f76712279a4260285461ef8f961044574
David Neukirchen <david.neukirchen@gmail.com>
2017-09-21 12:20:18 +0200

    Add rename icon to toolbar

--- administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
+++ administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
@@ -2,18 +2,22 @@
     <div class="media-toolbar">
         <media-breadcrumb></media-breadcrumb>
         <div class="media-view-icons">
-            <a href="#" class="media-toolbar-icon" @click="openRenameModal()">
-                <span class="fa fa-rename" aria-hidden="true"></span>
-            </a>
-            <a href="#" class="media-toolbar-icon" @click="changeListView()">
+            <transition name="fadeIn">
+                <a href="#" class="media-toolbar-icon" @click.stop="openRenameModal()" v-if="atLeastOneItemSelected">
+                    <span class="fa fa-text-width" aria-hidden="true"></span>
+                </a>
+            </transition>
+            <a href="#" class="media-toolbar-icon" @click.stop="changeListView()">
                 <span :class="toggleListViewBtnIcon" aria-hidden="true"></span>
             </a>
-            <a href="#" class="media-toolbar-icon" @click="toggleInfoBar">
+            <a href="#" class="media-toolbar-icon" @click.stop="toggleInfoBar">
                 <span class="fa fa-info" aria-hidden="true"></span>
             </a>
-            <a href="#" class="media-toolbar-icon" v-if="isLoading">
-                <span class="fa fa-spinner fa-spin" aria-hidden="true"></span>
-            </a>
+            <transition name="fadeIn">
+                <a href="#" class="media-toolbar-icon" v-if="isLoading">
+                    <span class="fa fa-spinner fa-spin" aria-hidden="true"></span>
+                </a>
+            </transition>
         </div>
     </div>
 </template>
@@ -29,6 +33,9 @@
             },
             isLoading() {
                 return this.$store.state.isLoading;
+            },
+            atLeastOneItemSelected() {
+                return this.$store.state.selectedItems.length > 0;
             }
         },
         methods: {
@@ -40,7 +47,7 @@
                 }
             },
             changeListView() {
-                if(this.$store.state.listView === 'grid') {
+                if (this.$store.state.listView === 'grid') {
                     this.$store.commit(types.CHANGE_LIST_VIEW, 'table');
                 } else {
                     this.$store.commit(types.CHANGE_LIST_VIEW, 'grid');
