163891b863830097ea292b5d7c1122912d92780b
Allon Moritz <allon.moritz@digital-peak.com>
2018-01-11 11:22:58 +0100

    Use the language string for the override message

--- administrator/components/com_media/resources/scripts/plugins/translate.js
+++ administrator/components/com_media/resources/scripts/plugins/translate.js
@@ -2,13 +2,31 @@
  * Translate plugin
  */
 
+import './../../../node_modules/sprintf-js/src/sprintf';
+
 let Translate = {};
+
+Translate.translate = function (key) {
+	// Translate from Joomla text
+	return Joomla.JText._(key, key);
+}
+
+Translate.sprintf = function (key) {
+	// Convert the arguments to array
+	var args = Array.prototype.slice.call(arguments);
+
+	// Remove the key
+	args.shift();
+
+	// Change the placeholders from the arguments
+	return vsprintf(this.translate(key), args);
+}
 
 Translate.install = function (Vue, options) {
 	Vue.mixin({
 		methods: {
 			translate: function (key) {
-				 return Joomla.JText._(key, key);
+				 return Translate.translate(key);
 			}
 		}
 	})
