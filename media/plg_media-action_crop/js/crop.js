4031fbbb85d76771c7bed85713a3cda9f7d64136
Ove Eriksson <tobby.eriksson@gmail.com>
2018-01-21 18:15:57 +0100

    Wait for the image to load

--- media/plg_media-action_crop/js/crop.js
+++ media/plg_media-action_crop/js/crop.js
@@ -59,19 +59,22 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 
 		var elements = [].slice.call(document.querySelectorAll(".crop-aspect-ratio-option"));
 
-		// Set default aspect ratio after numeric check, option has a dummy value
-		var defaultCropFactor = image.naturalWidth / image.naturalHeight;
-		if (!isNaN(+defaultCropFactor) && isFinite(defaultCropFactor)){
-			elements[0].value = defaultCropFactor;
-		}
+		// Wait for the image to load its data
+		setTimeout(function() {
+			// Set default aspect ratio after numeric check, option has a dummy value
+			var defaultCropFactor = image.naturalWidth / image.naturalHeight;
+			if (!isNaN(+defaultCropFactor) && isFinite(defaultCropFactor)){
+				elements[0].value = defaultCropFactor;
+			}
 			Joomla.MediaManager.Edit.crop.cropper.setAspectRatio(elements[0].value);
 
-		// Set click listener and function to the aspect ratio list
-		elements.forEach(function(element) {
-			element.addEventListener('click', function (e) {
-				Joomla.MediaManager.Edit.crop.cropper.setAspectRatio(this.value);
-  			});
-		});
+			// Set click listener and function to the aspect ratio list
+			elements.forEach(function(element) {
+				element.addEventListener('click', function (e) {
+					Joomla.MediaManager.Edit.crop.cropper.setAspectRatio(this.value);
+  				});
+			});
+		}, 2000);
 	};
 
 	// Register the Events
