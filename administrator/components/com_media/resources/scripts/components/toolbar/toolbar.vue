1a78b4dbe2b48358a5013b085e3d6c1bd72e0486
Geetanshu <mathur.geetanshu887@gmail.com>
2018-04-17 13:43:24 +0530

    Added translation string

--- administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
+++ administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
@@ -9,7 +9,7 @@
             </a>
         </div>
         <media-breadcrumb></media-breadcrumb>
-        <input type="text" @input="changeSearch" placeholder="Search"/>
+        <input type="Jtext" @input="changeSearch" :placeholder="translate('COM_MEDIA_SEARCH')"/>
         <div class="media-view-icons">
             <a href="#" class="media-toolbar-icon media-toolbar-decrease-grid-size"
                v-if="isGridView"
